a6729036583dbd23cd0cdbba69edcf8b7f616465
Shotadft <98450322+shotadft@users.noreply.github.com>
2026-05-24 14:28:21 +0900

    Add more

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import java.time.LocalDate
+
 plugins {
     `java-library`
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.maven.publish)
 }
 
 group = "com.shotadft"
@@ -14,9 +18,13 @@ repositories {
 dependencies {
     implementation(libs.jspecify)
 
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform("org.junit:junit-bom:6.1.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 java {
@@ -25,6 +33,56 @@ java {
     toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
 }
 
-tasks.test {
-    useJUnitPlatform()
+spotless {
+    java {
+        target("src/**/java/**/*.java")
+        licenseHeaderFile(rootProject.file("config/license-header.txt"))
+    }
+}
+
+val gitHubUserName = "shotadft"
+val gitHubRepoName = "KanaConverter"
+val projectName = project.rootProject.name
+    .split("-")
+    .joinToString("") { it.replaceFirstChar { c -> c.uppercase() } }
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = group as String,
+        artifactId = project.rootProject.name,
+        version = version as String
+    )
+
+    pom {
+        name.set(projectName)
+        description.set("This is a library that converts Roman letters to both hiragana and katakana and vice versa.")
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/${gitHubUserName}/KanaConverter")
+
+        licenses {
+            license {
+                name.set("The Apache Software License, Version 2.0")
+                url.set("https://www.apache.org.licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
+            }
+        }
+
+        developers {
+            developer {
+                id.set(gitHubUserName)
+                name.set("Shouta Fukaya")
+                email.set("98450322+shotadft@users.noreply.github.com")
+                url.set("https://github.com/${gitHubUserName}/")
+            }
+        }
+
+        scm {
+            connection = "scm:git:https://github.com/${gitHubUserName}/${gitHubRepoName}.git"
+            developerConnection = "scm:git:git@github.com:${gitHubUserName}/${gitHubRepoName}.git"
+            url = "https://github.com/${gitHubUserName}/$gitHubRepoName"
+        }
+    }
 }
--- src/test/java/com/shotadft/kanaconverter/test/TestClass.java
+++ src/test/java/com/shotadft/kanaconverter/test/TestClass.java
@@ -1,15 +1,36 @@
+/**
+ * Copyright 2026 Shotadft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.shotadft.kanaconverter.test;
 
 import com.shotadft.kanaconverter.KanaConverter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Instant;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestClass {
-    static void main(String[] args) throws IOException {
-        String input = IO.readln();
+    @Test
+    @DisplayName("ローマ字 -> ひらがな テスト")
+    public void convertTest() throws IOException {
+        String input = "irohanihoheto tirinuruwo wakayotareso tunenaramu einookuyama kefukoete asakiyumemisi eimosesu";
         KanaConverter converter = new KanaConverter();
 
         String result = converter.convert(input);
@@ -22,5 +43,7 @@ public class TestClass {
         }
 
         IO.println(result);
+
+        assertEquals("いろはにほへと ちりぬるを わかよたれそ つねならむ えいのおくやま けふこえて あさきゆめみし えいもせす", result);
     }
 }
